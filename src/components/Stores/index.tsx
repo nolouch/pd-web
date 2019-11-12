2aea8fe91f2e1bf4ea3a9264a07940b4c0c13e16
Yue Yang <g1enyy0ung@gmail.com>
2019-11-12 19:31:53 +0800

    Use component style

--- src/components/Stores/index.tsx
+++ src/components/Stores/index.tsx
@@ -1,17 +1,27 @@
-import React, { useEffect } from 'react'
+import React, { Component } from 'react'
 
+import { PDStoresAction } from 'types/stores'
+import { RootState } from 'reducers'
+import { ThunkDispatch } from 'redux-thunk'
+import { connect } from 'react-redux'
 import { getPDStores } from 'api/stores'
-import { useDispatch } from 'react-redux'
 
-const Stores = () => {
-  const dispatch = useDispatch()
-
-  useEffect(() => {
-    dispatch(getPDStores())
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
-
-  return <div className="PD-Stores"></div>
+interface StoresProps {
+  handleGetPDStores: () => void
 }
 
-export default Stores
+class Stores extends Component<StoresProps> {
+  componentDidMount() {
+    this.props.handleGetPDStores()
+  }
+
+  render() {
+    return <div className="PD-Stores"></div>
+  }
+}
+
+const mapDispatchToProps = (dispatch: ThunkDispatch<RootState, undefined, PDStoresAction>) => ({
+  handleGetPDStores: () => dispatch(getPDStores())
+})
+
+export default connect(null, mapDispatchToProps)(Stores)
