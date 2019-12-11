df8bf7a0171c470a0f1670f2b5bb21b1bf4c6546
Yue Yang <g1enyy0ung@gmail.com>
2019-12-11 15:41:45 +0800

    Handle API_URL in different situations

--- src/api/config.ts
+++ src/api/config.ts
@@ -3,9 +3,7 @@ import axios from 'axios'
 const API_URL = process.env.REACT_APP_PD_CLIENT_URL
 console.log('🔗PD_CLIENT_URL: ' + API_URL)
 const http = axios.create({
-  baseURL: `${API_URL}/pd/api/v1`
+  baseURL: `${process.env.NODE_ENV === 'development' ? API_URL || '' : ''}/pd/api/v1`
 })
 
-export {
-  http
-}
+export { http }
